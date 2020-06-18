76e9b3bd5f78aa0669c71bc1b6c29cbb774caf4a
f.t.luxton <f.t.luxton@student.saintkentigern.com>
2020-06-18 18:34:15 +1200

    test

--- Assessment.py
+++ Assessment.py
@@ -1,8 +1,4 @@
 # Interactive cryptography playground
-from GUI import Ui_Form
-from PyQt5 import QtCore, QtGui, QtWidgets
-from Vigenere import VigenereCypher
-from XOR import XOR
 
 
 class Cypher:
@@ -12,6 +8,3 @@ class Cypher:
         while user_input == '':
             user_input = Cypher.get_input(string)
         return user_input
-
-
-Cypher().run()
--- GUI.py
+++ GUI.py
@@ -11,43 +11,53 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
+
 class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(700, 500)
-        form_width_ratio = Form.width()/700
-        form_height_ratio = Form.height()/500
-        self.comboBox = QtWidgets.QComboBox(Form)
-        comboBox_base_width = 131
-        comboBox_base_height = 21
-        self.comboBox.setGeometry(QtCore.QRect(20, 20, form_width_ratio*comboBox_base_width, form_height_ratio*comboBox_base_height))
+    @staticmethod
+    def ratio_alter(ratio_width, ratio_height, x, y, width, height):
+        alter_x = ratio_width * x
+        alter_y = ratio_height * y
+        alter_width = ratio_width * width
+        alter_height = ratio_height * height
+        return QtCore.QRect(alter_x, alter_y, alter_width, alter_height)
+
+    def setup_ui(self, form):
+        form.setObjectName("Form")
+        form.resize(1000, 750)
+        form_width_ratio = form.width()/565
+        form_height_ratio = form.height()/399
+        self.comboBox = QtWidgets.QComboBox(form)
+        self.comboBox.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 20, 20, 131, 21))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(Form)
-        self.plainTextEdit.setGeometry(QtCore.QRect(180, 20, 361, 151))
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(form)
+        self.plainTextEdit.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 180, 20, 361, 151))
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(Form)
-        self.plainTextEdit_2.setGeometry(QtCore.QRect(20, 90, 131, 281))
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(form)
+        self.plainTextEdit_2.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 20, 90, 131, 281))
         self.plainTextEdit_2.setObjectName("plainTextEdit_2")
-        self.plainTextEdit_3 = QtWidgets.QPlainTextEdit(Form)
-        self.plainTextEdit_3.setGeometry(QtCore.QRect(180, 190, 361, 181))
+        self.plainTextEdit_3 = QtWidgets.QPlainTextEdit(form)
+        self.plainTextEdit_3.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 180, 190, 361, 151))
         self.plainTextEdit_3.setObjectName("plainTextEdit_3")
-        self.radioButton = QtWidgets.QRadioButton(Form)
-        self.radioButton.setGeometry(QtCore.QRect(20, 60, 62, 14))
+        self.radioButton = QtWidgets.QRadioButton(form)
+        self.radioButton.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio,20, 60, 62, 14))
         self.radioButton.setChecked(True)
         self.radioButton.setObjectName("radioButton")
-        self.radioButton_2 = QtWidgets.QRadioButton(Form)
-        self.radioButton_2.setGeometry(QtCore.QRect(90, 60, 62, 14))
+        self.radioButton_2 = QtWidgets.QRadioButton(form)
+        self.radioButton_2.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 90, 60, 62, 14))
         self.radioButton_2.setObjectName("radioButton_2")
+        self.pushButton = QtWidgets.QPushButton(form)
+        self.pushButton.setGeometry(self.ratio_alter(form_width_ratio, form_height_ratio, 479, 360, 62, 14))
+        self.pushButton.setObjectName("pushButton")
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslate_ui(form)
+        QtCore.QMetaObject.connectSlotsByName(form)
 
-    def retranslateUi(self, Form):
+    def retranslate_ui(self, form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        form.setWindowTitle(_translate("Form", "Form"))
         self.comboBox.setItemText(0, _translate("Form", "Caeser Cypher"))
         self.comboBox.setItemText(1, _translate("Form", "Vigenere Cypher"))
         self.comboBox.setItemText(2, _translate("Form", "XOR Algorithm"))
@@ -56,13 +66,13 @@ class Ui_Form(object):
         self.plainTextEdit_3.setPlainText(_translate("Form", "Resulting text"))
         self.radioButton.setText(_translate("Form", "Ecrypt"))
         self.radioButton_2.setText(_translate("Form", "Decrypt"))
+        self.pushButton.setText(_translate("Form", "Run"))
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
-    ui.setupUi(Form)
+    ui.setup_ui(Form)
     Form.show()
     sys.exit(app.exec_())
-
