a82021c90995e6ee1fd7341b5c15b958a2509ae2
f.t.luxton <f.t.luxton@student.saintkentigern.com>
2020-06-24 08:28:56 +1200

    Changelist

--- GUI.py
+++ GUI.py
@@ -16,6 +16,10 @@ from Assessment import Cypher
 
 class UiForm(object):
     def __init__(self, form):
+        # Forming initial ratios
+        self.width_ratio = 1
+        self.height_ratio = 1
+        # Establishing base widgets
         self.comboBox = QtWidgets.QComboBox(form)
         self.plainTextEdit = QtWidgets.QPlainTextEdit(form)
         self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(form)
@@ -24,43 +28,58 @@ class UiForm(object):
         self.radioButton_2 = QtWidgets.QRadioButton(form)
         self.pushButton = QtWidgets.QPushButton(form)
 
-    @staticmethod
-    def ratio_alter(ratio_width, ratio_height, x, y, width, height):
-        alter_x = ratio_width * x
-        alter_y = ratio_height * y
-        alter_width = ratio_width * width
-        alter_height = ratio_height * height
+    def ratio_alter(self, x, y, width, height):
+        # Returns coordinates and dimensions altered by the form size
+        alter_x = self.width_ratio * x
+        alter_y = self.height_ratio * y
+        alter_width = self.width_ratio * width
+        alter_height = self.height_ratio * height
         return QtCore.QRect(alter_x, alter_y, alter_width, alter_height)
 
     def setup_ui(self, form):
         def setup_widget(self_name, geometry, name):
+            # Quick setup widget to save space and time
             self_name.setGeometry(geometry)
             self_name.setObjectName(name)
-
+        # Creating the form
         form.setObjectName("Form")
         form.resize(1000, 750)
-        form_width_ratio = form.width()/565
-        form_height_ratio = form.height()/399
-        setup_widget(self.comboBox, self.ratio_alter(form_width_ratio, form_height_ratio, 20, 20, 131, 21), 'comboBox')
+        # Altering the ratios
+        self.width_ratio = form.width()/565
+        self.height_ratio = form.height()/399
+        # Creating the widgets
+        setup_widget(self.comboBox, self.ratio_alter(20, 20, 131, 21), 'comboBox')
+        setup_widget(self.plainTextEdit, self.ratio_alter(180, 20, 361, 151), 'plainTextEdit')
+        setup_widget(self.plainTextEdit_2, self.ratio_alter(20, 90, 131, 281), 'plainTextEdit_2')
+        setup_widget(self.plainTextEdit_3, self.ratio_alter(180, 190, 361, 151), 'plainTextEdit_3')
+        setup_widget(self.radioButton, self.ratio_alter(20, 60, 62, 14), 'radioButton')
+        setup_widget(self.radioButton_2, self.ratio_alter(90, 60, 62, 14), 'radioButton_2')
+        setup_widget(self.pushButton, self.ratio_alter(479, 360, 62, 14), 'pushButton')
+        # Adding spaces for the comboBox
         for i in range(len(Cypher().cyphers)):
             self.comboBox.addItem("")
-        setup_widget(self.plainTextEdit, self.ratio_alter(form_width_ratio, form_height_ratio, 180, 20, 361, 151), 'plainTextEdit')
-        setup_widget(self.plainTextEdit_2, self.ratio_alter(form_width_ratio, form_height_ratio, 20, 90, 131, 281), 'plainTextEdit_2')
-        setup_widget(self.plainTextEdit_3, self.ratio_alter(form_width_ratio, form_height_ratio, 180, 190, 361, 151), 'plainTextEdit_3')
+        # Setting options for widgets
         self.plainTextEdit_3.setReadOnly(True)
-        setup_widget(self.radioButton, self.ratio_alter(form_width_ratio, form_height_ratio, 20, 60, 62, 14), 'radioButton')
         self.radioButton.setChecked(True)
-        setup_widget(self.radioButton_2, self.ratio_alter(form_width_ratio, form_height_ratio, 90, 60, 62, 14), 'radioButton_2')
-        setup_widget(self.pushButton, self.ratio_alter(form_width_ratio, form_height_ratio, 479, 360, 62, 14), 'pushButton')
+        # Binding the run button to the cypher_text function
         self.pushButton.clicked.connect(self.cypher_text)
+        # Updating the widget text
         self.retranslate_ui(form)
+        # Connecting slots to the form
         QtCore.QMetaObject.connectSlotsByName(form)
 
     def cypher_text(self):
+        # Establishing variables to save space
         inputs = [self.plainTextEdit.toPlainText(), self.plainTextEdit_2.toPlainText()]
-        self.plainTextEdit_3.setPlainText(Cypher().cyphers[self.comboBox.currentText()](inputs[0], inputs[1]).encrypt())
+        encryption = Cypher().cyphers[self.comboBox.currentText()](inputs[0], inputs[1])
+        # Encrypts or decrypts depending on selected radioButton
+        if self.radioButton.isChecked():
+            self.plainTextEdit_3.setPlainText(encryption.encrypt())
+        else:
+            self.plainTextEdit_3.setPlainText(encryption.decrypt())
 
     def retranslate_ui(self, form):
+        # Updating all text
         _translate = QtCore.QCoreApplication.translate
         form.setWindowTitle(_translate("Form", "Cypher Playground"))
         for i, cypher in enumerate(Cypher().cyphers):
@@ -74,6 +93,7 @@ class UiForm(object):
 
 
 if __name__ == '__main__':
+    # Running the UI
     app = QtWidgets.QApplication(sys.argv)
     form = QtWidgets.QWidget()
     ui = UiForm(form)
